485b23e022f38e38bfa1ff5bee94968a863a03c6
liupjie <374299590@qq.com>
2021-08-04 18:31:13 +0800

    tab format

--- src/test/java/org/apache/ibatis/MyTest.java
+++ src/test/java/org/apache/ibatis/MyTest.java
@@ -15,24 +15,29 @@ import java.util.Map;
 
 public class MyTest extends BaseDataTest {
 
-  @BeforeAll
-  static void setup() throws Exception {
-    createBlogDataSource();
-  }
-
-  @Test
-  public void test1() throws IOException {
-    final String resource = "org/apache/ibatis/builder/MapperConfig.xml";
-    final Reader reader = Resources.getResourceAsReader(resource);
-    SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
-    SqlSession sqlSession = sqlSessionFactory.openSession();
-    BlogMapper mapper = sqlSession.getMapper(BlogMapper.class);
-    List<Map> maps = mapper.selectAllPosts();
-    for (Map map : maps) {
-      map.forEach((k, v) -> {
-        System.out.println(k + ":" + v);
-      });
+    @BeforeAll
+    static void setup() throws Exception {
+        createBlogDataSource();
     }
-  }
+
+    @Test
+    public void test1() throws IOException {
+        final String resource = "org/apache/ibatis/builder/MapperConfig.xml";
+        final Reader reader = Resources.getResourceAsReader(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        BlogMapper mapper = sqlSession.getMapper(BlogMapper.class);
+        List<Map> maps = mapper.selectAllPosts();
+        for (Map map : maps) {
+            map.forEach((k, v) -> {
+                System.out.println(k + ":" + v);
+            });
+        }
+    }
+
+    @Test
+    public void localDBTest1() {
+
+    }
 
 }
